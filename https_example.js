32977529d9be724d2315d3e3a626c6f9a7d4ae41
Zeyu Liu <haozai_03@hotmail.com>
2019-03-11 04:14:19 +0000

    chunk output

--- https_example.js
+++ https_example.js
@@ -9,7 +9,11 @@ var options = {
 
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");

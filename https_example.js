0e93f09ea8de8f40305dca8ac6de402ffc68def5
Zeyu Liu <haozai_03@hotmail.com>
2019-03-11 04:03:23 +0000

    making request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,17 @@
 var https = require('https');
 
-console.log('i did it')
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
